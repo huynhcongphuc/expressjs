ecf70f7d1095ef3ebb839f06f0571eeaf8f36433
huynhcongphuc <phuchuynh247@gmail.com>
2021-09-28 20:18:08 +0700

    m

---
+++ index.js
@@ -0,0 +1,10 @@
+const express  = require('express');
+const app = express();
+const port = 5000;
+app.get('/', (req, res) =>{
+    return res.send("hello World");
+});
+
+app.listen(port,() => {
+    console.log(`listening on port: ${port}`);
+});
