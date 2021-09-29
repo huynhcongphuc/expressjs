2b20895e9f7690fd1c1c94fc32135961ff165749
huynhcongphuc <phuchuynh247@gmail.com>
2021-09-29 09:36:15 +0700

    a

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express  = require('express');
 const app = express();
 const port = 5000;
-app.get('/', (req, res) =>{
+app.get('/test', (req, res) =>{
     return res.send("hello World");
 });
 
