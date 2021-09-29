be6391db3e96b524137c0eb982ed4a55c245bd85
huynhcongphuc <phuchuynh247@gmail.com>
2021-09-29 20:33:13 +0700

    a

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 5000;
 
 //http logger
-app.use(morgan('combined'));
+//app.use(morgan('combined'));
 
 //Template Engines
 app.engine('handlebars', expresshandle());
@@ -14,8 +14,9 @@ app.set('view engine', 'handlebars')
 app.set('views','./src/resources/views/');
 
 app.use(express.static('./src/public'));
-
-
+//su dung midleware su ly du lieu post form
+app.use(express.urlencoded());
+app.use(express.json());
 //render page with Template Engines
 app.get('/', (req, res) =>{
     res.render('home');
@@ -24,6 +25,17 @@ app.get('/', (req, res) =>{
 
 app.get('/news', (req, res) =>{
     res.render('news');
+    //return res.send("hello");
+});
+
+app.post('/news', (req, res) =>{
+    console.log(req.body.gender);
+    res.send("");
+    //return res.send("hello");
+});
+
+app.get('/search', (req, res) =>{
+    res.render('search');
     //return res.send("hello");
 });
 
